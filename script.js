5bc476e63f30d42081cde813058e8af423baa2ef
Ziyodabonu <159676375+ziyoda-art@users.noreply.github.com>
2025-06-29 13:41:03 +0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // ============ TOGGLE MENU ============
 function toggleMenu() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("active");
+  const navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+}
+// ============ NAVBAR SCROLL EFFECT ============
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
   }
-  // ============ NAVBAR SCROLL EFFECT ============
-  window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
+});
+// video uchun play
+const video = document.getElementById('myVideo');
+const overlay = document.getElementById('overlay');
+
+function playVideo() {
+  video.play();
+  overlay.style.display = 'none';
+}
