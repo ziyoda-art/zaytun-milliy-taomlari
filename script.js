1ce534d9b4b85ed49729de3983d335ce6a759218
Ziyodabonu <159676375+ziyoda-art@users.noreply.github.com>
2025-06-30 12:22:08 +0300

    \Ziyoda

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function toggleMenu() {
       navbar.classList.remove("scrolled");
     }
   });
+
+    // menu 
+    document.addEventListener("DOMContentLoaded", function () {
+      const items = document.querySelectorAll(".menu-item");
+      const showMoreBtn = document.getElementById("show-more-btn");
+      let expanded = false; // состояние меню
+  
+      // Сначала показываем только первые 3
+      items.forEach((item, index) => {
+        if (index > 2) {
+          item.classList.add("hidden");
+        }
+      });
+  
+      // Обработчик нажатия кнопки
+      showMoreBtn.addEventListener("click", () => {
+        expanded = !expanded;
+  
+        items.forEach((item, index) => {
+          if (index > 2) {
+            item.classList.toggle("hidden", !expanded);
+          }
+        });
+  
+        // Меняем текст кнопки
+        showMoreBtn.textContent = expanded ? "Yopish" : "To‘liq menyuni ko‘rish";
+      });
+    });
+  
